beafbda2fe06f06585b6881b238eca1fb21b5f61
Patty8122 <divyapattisapu@uchicago.edu>
2024-04-15 19:31:01 -0400

    add seq code and timer

--- task2/testid.sh
+++ task2/testid.sh
@@ -39,6 +39,12 @@ echo 0 > "/var/tmp/current_id"
 
 NUM_PROCESSES=$1
 
+if [ "$2" == "seq" ]; then
+    seq_flag=1
+else
+    seq_flag=0
+fi
+
 # remove the file if it exists
 if [ -f "ids.txt" ]; then
     rm "ids.txt"
@@ -47,6 +53,17 @@ fi
 # create the file
 touch "ids.txt"
 
+start=`date +%s%N`
+if [ $seq_flag -eq 1 ]; then
+    for ((i=1; i<=$NUM_PROCESSES; i++)); do
+        echo $i >> "ids.txt"
+    done
+    end=`date +%s%N`
+    echo "Sequential generation took $((end-start)) nanoseconds."
+    exit 0
+fi
+
+
 # call genid concurrently
 for ((i=0; i<$NUM_PROCESSES; i++)); do
     genid >> "ids.txt" &
@@ -54,11 +71,15 @@ done
 
 wait
 
+end=`date +%s%N`
+echo "Concurrent generation took $((end-start)) nanoseconds."
+
 # check if they are equal to (seq 1 NUM_PROCESSES)
 if [ $(sort -n "ids.txt" | uniq | wc -l) -eq $NUM_PROCESSES ]; then
     echo "id.txt contains the generated sequential numbers from 1 to $NUM_PROCESSES"
 else
     echo "Some ids are not unique or not sequential. Check ids.txt for more information."
 fi 
+
 
 
